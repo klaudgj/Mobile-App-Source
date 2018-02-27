48695788033ade49639305e019e779ae00497aff
Klaud <klaudgj@gmail.com>
2018-02-27 16:33:20 +0100

    Initial commit

---
+++ src/assets/js/settings.js
@@ -0,0 +1,12 @@
+var wordpress_url = "http://yourdomain.com";
+var wordpress_per_page = 10;
+var request_timeout = 30000;
+var open_target_blank = true;
+var date_format = 'd MMM y HH:mm';
+var onesignal_app_id = '';
+var application_language = 'en';
+var google_analytics = ''; 
+var admob_android_banner = '';
+var admob_android_interstitial = '';
+var admob_ios_banner = '';
+var admob_ios_interstitial = '';
